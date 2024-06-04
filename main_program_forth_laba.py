74d788de70e604bf42a9983b88084b78a7d7a067
Stepashka( <Egoborshka@mail.ru>
2024-06-04 10:29:27 +0300

    Add file

---
+++ main_program_forth_laba.py
@@ -0,0 +1,7 @@
+import module_forth_laba
+
+result = module_forth_laba.add_numbers(5, 3)
+print("Результат сложения: ", result)
+
+description = module_forth_laba.module_description()
+print("Описание модуля: ", description)
---
+++ module_forth_laba.py
@@ -0,0 +1,5 @@
+def add_numbers(a, b):
+    return a + b
+
+def module_description():
+    return "Этот модуль содержит функцию для сложения двух чисел."
